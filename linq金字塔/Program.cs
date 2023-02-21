cfe4910d6892f5c155644d196965996250f37884
dxsxryan <dxsxryan@gmail.com>
2023-02-21 12:46:30 +0800

    根本不知道自己在寫什麼 但是成功了

---
+++ linq金字塔/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq金字塔
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入一個正整數：");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            var seq = Enumerable.Range(1, n);
+            var pyramid = seq.Select(i => new string(Convert.ToChar(n - i + 49), i));
+
+            foreach (var line in pyramid)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
