ed610d107108312c25ef06178e1afd785e1111ce
dxsxryan <dxsxryan@gmail.com>
2023-02-21 11:16:45 +0800

    還沒排版 但功能完成了

---
+++ 奇偶判斷/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 奇偶判斷
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入：");
+            var list = new List<string>() { "8", "7", "9", "3", "11", "6", "2", "18" };
+            var odd = new List<string>();
+            var even = new List<string>();
+            foreach(var item in list)
+            {
+                if(item != list.Last())
+                {
+                    Console.Write($"{item}, ");
+                }
+                else
+                {
+                    Console.Write(item);
+                }
+            }
+            foreach(var item in list)
+            {
+                if (int.Parse(item) % 2 == 0)
+                {
+                    even.Add(item);
+                }
+                else
+                {
+                    odd.Add(item);
+                }
+            }
+            Console.Write("奇數：");
+            foreach (var item in odd)
+            {
+                if (item != odd.Last())
+                {
+                    Console.Write($"{item}, ");
+                }
+                else
+                {
+                    Console.Write(item);
+                }
+            }
+            foreach (var item in even)
+            {
+                if (item != even.Last())
+                {
+                    Console.Write($"{item}, ");
+                }
+                else
+                {
+                    Console.Write(item);
+                }
+            }
+        }
+    }
+}
